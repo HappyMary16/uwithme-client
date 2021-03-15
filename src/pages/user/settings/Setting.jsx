0d95399dfd8bd3b39a23ed02dd6f893077334e7b
mborodin <m.borodin.1999@gmail.com>
2021-03-16 00:18:30 +0200

    some changes in Setting.jsx

--- src/pages/user/settings/Setting.jsx
+++ src/pages/user/settings/Setting.jsx
@@ -13,6 +13,21 @@ import { loadDepartment } from '../../../actions/departmentActions';
 import { loadGroup } from '../../../actions/groupActions';
 
 class Setting extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isEditMode: false
+    };
+
+    this.handleGroupChange = this.handleGroupChange.bind(this);
+    this.endEditing = this.endEditing.bind(this);
+    this.startEdit = this.startEdit.bind(this);
+    this.openDeleteLessonDialog = this.openDeleteLessonDialog.bind(this);
+    this.closeDeleteLessonDialog = this.closeDeleteLessonDialog.bind(this);
+    this.deleteLesson = this.deleteLesson.bind(this);
+  }
+
   componentDidMount() {
     const { dispatch, user } = this.props;
     dispatch(loadUniversity());
@@ -29,6 +44,7 @@ class Setting extends Component {
 
   render() {
     const { groups, departments, institutes, universities, user } = this.props;
+    const { isEditMode } = this.state;
     const { role, university, institute, department, group } = user;
 
     return (
@@ -40,18 +56,21 @@ class Setting extends Component {
               onChange={e => this.setState({ universityName: e.target.value })}
               defaultValue={user.firstName}
               required
+              readOnly={!isEditMode}
             />
             <Form.Label>{i18n.t('last_name')}</Form.Label>
             <Form.Control
               onChange={e => this.setState({ universityName: e.target.value })}
               defaultValue={user.lastname}
               required
+              readOnly={!isEditMode}
             />
             <Form.Label>{i18n.t('surname')}</Form.Label>
             <Form.Control
               onChange={e => this.setState({ universityName: e.target.value })}
               defaultValue={user.surname}
               required
+              readOnly={!isEditMode}
             />
             <Form.Label>{i18n.t('email')}</Form.Label>
             <Form.Control
@@ -59,6 +78,7 @@ class Setting extends Component {
               onChange={e => this.setState({ universityName: e.target.value })}
               defaultValue={user.email}
               required
+              readOnly={!isEditMode}
             />
             <Form.Label>{i18n.t('password')}</Form.Label>
             <Form.Control
@@ -66,6 +86,7 @@ class Setting extends Component {
               onChange={e => this.setState({ universityName: e.target.value })}
               defaultValue={user.password}
               required
+              readOnly={!isEditMode}
             />
             <Form.Label>{i18n.t('university')}</Form.Label>
             <Select
@@ -107,13 +128,22 @@ class Setting extends Component {
                 />
               </div>
             )}
-            <Button block variant={'purple'} type={'submit'}>
-              {i18n.t('edit')}
-            </Button>
-            <Button block variant={'red'} type={'submit'}>
-              {i18n.t('delete')}
-            </Button>
+            {isEditMode && (
+              <Button block variant={'purple'} type={'submit'}>
+                {i18n.t('save')}
+              </Button>
+            )}
           </Form>
+          {!isEditMode && (
+            <div>
+              <Button block variant={'purple'} type={'submit'}>
+                {i18n.t('edit')}
+              </Button>
+              <Button block variant={'red'} type={'submit'}>
+                {i18n.t('delete')}
+              </Button>
+            </div>
+          )}
         </Col>
       </Row>
     );
