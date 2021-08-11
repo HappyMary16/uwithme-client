1ff980b52cac04488ba946dec9048eb7c981323f
mariia.borodin <m.borodin.1999@gmail.com>
2021-08-11 12:27:39 +0300

    update icons

--- src/pages/icons/DepartmentIcon.jsx
+++ src/pages/icons/DepartmentIcon.jsx
@@ -6,23 +6,19 @@ export const DepartmentIcon = () => {
       <svg
         version="1.0"
         xmlns="http://www.w3.org/2000/svg"
-        height="1.8em"
-        viewBox="0 0 64.000000 64.000000"
+        height="1.7em"
+        viewBox="0 0 159.1 159.1"
         preserveAspectRatio="xMidYMid meet"
       >
-        <g
-          transform="translate(0.000000,64.000000) scale(0.100000,-0.100000)"
-          stroke="none"
-        >
-          <path
-            d="M170 511 l-135 -79 -3 -171 -2 -171 290 0 290 0 -2 171 -3 172 -135
-78 c-74 43 -142 79 -150 79 -8 0 -76 -36 -150 -79z m275 -30 l120 -68 3 -142
-3 -141 -31 0 -30 0 0 80 0 80 -80 0 -80 0 0 -80 0 -80 -140 0 -140 0 0 139 0
-140 122 70 c68 39 125 71 128 71 3 0 59 -31 125 -69z"
+        <g id="Department">
+          <polygon
+            className="st0"
+            points="79.6,16 146.5,54.7 146.5,132 79.6,132.1 12.7,132 12.7,54.7 	"
           />
-          <path d="M140 390 l0 -50 80 0 80 0 0 50 0 50 -80 0 -80 0 0 -50z" />
-          <path d="M350 390 l0 -50 80 0 80 0 0 50 0 50 -80 0 -80 0 0 -50z" />
-          <path d="M140 240 l0 -50 80 0 80 0 0 50 0 50 -80 0 -80 0 0 -50z" />
+          <rect x="87.3" y="87.6" className="st2" width="39.8" height="44.5" />
+          <rect x="87.3" y="51" className="st1" width="39.8" height="23.4" />
+          <rect x="34.9" y="87.6" className="st1" width="39.8" height="23.4" />
+          <rect x="34.9" y="51" className="st1" width="39.8" height="23.4" />
         </g>
       </svg>
     </div>
--- src/pages/icons/InstituteIcon.jsx
+++ src/pages/icons/InstituteIcon.jsx
@@ -14,7 +14,7 @@ export const InstituteIcon = () => {
           className="st0"
           points="113.6,19.1 212.8,76.4 212.8,190.8 113.6,191.1 14.5,190.8 14.5,76.4 	"
         />
-        <rect x="93.7" y="142.7" className="st1" width="39.8" height="48.4" />
+        <rect x="93.7" y="142.7" className="st2" width="39.8" height="48.4" />
         <rect x="93.7" y="106.1" className="st1" width="39.8" height="23.4" />
         <rect x="93.7" y="69.5" className="st1" width="39.8" height="23.4" />
         <rect x="41.3" y="142.7" className="st1" width="39.8" height="23.4" />
