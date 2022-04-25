0d17b56b7937f380e05b495db927091ab9adb930
Mikhail Groshevoy <developer.misha999777@gmail.com>
2022-04-26 01:31:44 +0300

    reverted workbox changes

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+/* config-overrides.js */
+const { rewireWorkboxGenerate } = require("react-app-rewire-workbox");
+
+module.exports = function override(config, env) {
+    if (env === "production") {
+        console.log("Production build - Adding Workbox for PWAs");
+        config = rewireWorkboxGenerate()(config, env);
+    }
+    return config;
+};
