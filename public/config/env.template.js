3fe06cc0e0189a59b344d7b7f8af4430b216642b
Mykhailo Hroshevyi <misha999777@gmail.com>
2022-11-26 12:23:29 +0100

    updated properties

--- public/config/env.template.js
+++ public/config/env.template.js
@@ -2,5 +2,5 @@ window["env"] = window["env"] || {};
 
 window["env"]["REACT_APP_SERVER_URL"] = "https://${UWITHME_BACKEND_HOST}";
 window["env"]["REACT_APP_AUTHORITY"] = "https://${UWITHME_KEYCLOAK_HOST}/realms/${UWITHME_KEYCLOAK_REALM}";
-window["env"]["REACT_APP_TEST_SYSTEM"] = "${UWITHME_TESTSYSTEM_URL}";
+window["env"]["REACT_APP_TEST_SYSTEM"] = "https://${UWITHME_TESTSYSTEM_URL}";
 window["env"]["REACT_APP_TELEGRAM_BOT"] = "https://t.me/${UWITHME_BOT_NAME}";
