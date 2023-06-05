aa50a51b81e4ad125273931cc48c079a080e3566
mborodin <m.borodin.1999@gmail.com>
2023-06-05 15:46:21 +0200

    fixed bot notification

--- src/pages/bot/BotNotification.jsx
+++ src/pages/bot/BotNotification.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {Notification} from '../common/components/Notification';
-import {SmallAvatar} from '../common/components/SmallAvatar';
-import {Col, Row} from 'react-bootstrap';
+import {Toast, ToastContainer} from 'react-bootstrap';
 import {TELEGRAM_BOT_URI} from '../../config';
 import {botShown, selectBotShown} from "../../store/bot/botSlice";
 import {useTranslation} from "react-i18next";
+import logo from '../../assets/logo192.png'
 
 export default function BotNotification() {
 
@@ -19,24 +18,19 @@ export default function BotNotification() {
   }
 
   return (
-    <Notification
-      header={
-        <Row>
-          <Col xs={3} className="d-flex align-items-center">
-            <SmallAvatar image={'/logo192.png'}/>
-          </Col>
-          <Col className="d-flex align-items-center">
-            <p>{t('bot_notification_header')}</p>
-          </Col>
-        </Row>}
-      text={
-        <div>
-          <p>{t('bot_notification_text') + ' '}</p>
-          <a href={TELEGRAM_BOT_URI} rel="noopener noreferrer">
-            @{TELEGRAM_BOT_URI.substring(TELEGRAM_BOT_URI.lastIndexOf('/') + 1)}
-          </a>
-        </div>}
-      show={!wasBotNotificationShown}
-      onClose={closeMessage}/>
+      <ToastContainer position="bottom-end" className="p-3">
+        <Toast show={!wasBotNotificationShown} onClose={closeMessage} delay={0} animation={false}>
+          <Toast.Header>
+            <img src={logo} width={30} className="me-2" alt="logo"/>
+            <strong className="me-auto"><p>{t('bot_notification_header')}</p></strong>
+          </Toast.Header>
+          <Toast.Body>
+            <p>{t('bot_notification_text') + ' '}</p>
+            <a href={TELEGRAM_BOT_URI} rel="noopener noreferrer">
+              @{TELEGRAM_BOT_URI.substring(TELEGRAM_BOT_URI.lastIndexOf('/') + 1)}
+            </a>
+          </Toast.Body>
+        </Toast>
+      </ToastContainer>
   );
 }
--- src/pages/common/components/SmallAvatar.jsx
+++ src/pages/common/components/SmallAvatar.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import {useAvatarDownloader} from "../../../hooks/useAvatarDownloader";
 import emptyAvatar from "../../../assets/empty-avatar.png"
 
-export function SmallAvatar({size = "30px", user, image}) {
+export function SmallAvatar({size = "30px", user}) {
   const [avatar] = useAvatarDownloader(user?.id);
 
   return (
     <Image roundedCircle
            alt="photo"
-           src={avatar ?? image ?? emptyAvatar}
+           src={avatar ?? emptyAvatar}
            width={size}
            height={size}/>
   );
